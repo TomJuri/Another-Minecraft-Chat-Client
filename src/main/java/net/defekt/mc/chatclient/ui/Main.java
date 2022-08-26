ec1a66383bc7577fa1cbdf2f087efc9c717f940d
Defective4 <def3ctive4@gmail.com>
2022-08-26 09:06:50 +0200

    Bump version to 1.8.0

--- src/main/java/net/defekt/mc/chatclient/ui/Main.java
+++ src/main/java/net/defekt/mc/chatclient/ui/Main.java
@@ -146,7 +146,7 @@ public class Main {
 
     private static BufferedImage logoImage = null;
 
-    public static final String VERSION = "1.7.1";
+    public static final String VERSION = "1.8.0";
     private static final String CHANGELOG_URL = "https://raw.githubusercontent.com/Defective4/Another-Minecraft-Chat-Client/master/Changes";
 
     public static Font mcFont = Font.decode(null);
