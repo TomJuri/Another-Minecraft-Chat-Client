e1fbddd548cbb7be4e1f83371acec44408178375
Defective4 <def3ctive4@gmail.com>
2022-09-03 08:58:12 +0200

    Fix version

--- src/main/java/net/defekt/mc/chatclient/ui/Main.java
+++ src/main/java/net/defekt/mc/chatclient/ui/Main.java
@@ -165,7 +165,7 @@ public class Main {
 
     private static BufferedImage logoImage = null;
 
-    public static final String VERSION = "1.8.1";
+    public static final String VERSION = "1.8.2";
     private static final String CHANGELOG_URL = "https://raw.githubusercontent.com/Defective4/Another-Minecraft-Chat-Client/master/Changes";
 
     public static Font mcFont = Font.decode(null);
